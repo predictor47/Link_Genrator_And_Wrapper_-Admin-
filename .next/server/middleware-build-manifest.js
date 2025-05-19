5db2596d30bd014e61b59d6ad3112b89cbaefbb1
Developer <dev@protegeresearchsurvey.com>
2025-05-19 21:01:56 +0500

    Add admin and login pages; update build and cache files
    - Added new admin and login pages to the server and static chunks
    - Updated build-manifest, pages-manifest, and trace files
    - Refreshed Webpack cache and hot-update files
    - Modified middleware and vendor chunks for new routes

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -23,6 +23,16 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
+    ],
+    "/admin": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/admin.js"
+    ],
+    "/admin/login": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/admin/login.js"
     ]
   },
   "ampFirstPages": []
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3f5990eba800a235"; }
+/******/ 		__webpack_require__.h = function() { return "f18b828d57fe0c50"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
